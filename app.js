e6eda13215bc58831d530dff4b3c020b26f884c4
karthikbadam <sbadam@purdue.edu>
2015-01-31 20:34:17 -0500

    temporal

---
+++ app.js
@@ -0,0 +1,136 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+var path = require('path');
+var url = require('url');
+var csv = require('fast-csv');
+var fs = require('fs');
+var Parallel = require('paralleljs');
+
+var Stock = require('./Stock.js');
+
+
+//SOM
+var SOM = require('ml-som');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('html', require('ejs').renderFile);
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/* listen */
+var httpserver = http.createServer(app);
+httpserver.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+/* Initializing a self organizing map */
+
+/* reading the files */
+
+//start with internet information providers
+var stream = fs.createReadStream("data/iip.csv");
+
+var csvStream = csv
+    .fromStream(stream, {headers : true})
+    .on("data", function(data){
+        //console.log(data);
+
+        //for each symbol
+        var companyName = data.company;
+        var symbol = data.symbols;
+
+        var stockObject = new Stock({
+            company: companyName,
+            symbol: symbol
+        });
+
+        stockObject.readStock();
+
+        //stockObject.trainTemporal();
+
+//        var p = new Parallel(data);
+//
+//        p.spawn(function (data) {
+//
+//            //for each symbol
+//            var companyName = data.company;
+//            var symbol = data.symbols;
+//
+//            var stockObject = new Stock({
+//                company: companyName,
+//                symbol: symbol
+//            });
+//
+//            stockObject.readStock();
+//
+//            return stockObject.trainTemporal()
+//
+//        }).then(function (data) {
+//
+//            console.log(data);
+//
+//        });
+
+    })
+    .on("end", function(){
+        console.log("done");
+    });
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
+
---
+++ stock.js
@@ -0,0 +1,152 @@
+/**
+ * Created by karthik on 1/31/15.
+ */
+
+var csv = require('fast-csv');
+var fs = require('fs');
+var d3 = require('d3');
+var brain = require('brain');
+
+var parseDate =  d3.time.format("%Y%m%d").parse;
+var TEMPORAL_INPUT_SIZE = 7;
+var TEMPORAL_OUTPUT_SIZE = 2;
+
+function Stock (options) {
+    var _self = this;
+    _self.companyName = options.company;
+    _self.symbol = options.symbol;
+    _self.data = [];
+
+    _self.min = 100000;
+    _self.max = -100000;
+
+    _self.temporalNet = new brain.NeuralNetwork({
+        hiddenLayers: [40, 50, 20],
+        learningRate: 0.3 // global learning rate, useful when training using streams
+    });
+}
+
+Stock.prototype.readStock = function () {
+
+    var _self = this;
+
+    var stream = fs.createReadStream("data/"+_self.symbol+".csv");
+
+    var csvStream = csv
+        .fromStream(stream, {headers : true})
+        .on("data", function(d){
+
+            d["date"] = parseDate(String(d["date"]));
+            d["close price"] = +d["close price"];
+            d["volume"] = +d["volume"];
+
+            var close = d["close price"];
+
+            if (_self.min > close) {
+                _self.min = close;
+            }
+
+            if (_self.max < close) {
+                _self.max = close;
+            }
+
+            _self.data.push(d);
+
+
+        })
+        .on("end", function(){
+
+            console.log("done: "+_self.symbol+", size: "+_self.data.length);
+
+            for (var i = 0; i < _self.data.length; i++) {
+                if (_self.max - _self.min != 0)
+                    _self.data[i].normalized = (_self.data[i]["close price"] - _self.min)/(_self.max - _self.min);
+                else
+                    _self.data[i].normalized = 0;
+
+                //console.log(_self.data[i].normalized);
+            }
+
+            _self.trainTemporal();
+        });
+
+}
+
+Stock.prototype.getTemporalNetworkInput = function () {
+
+    //PAST SIX DAYS
+
+    var _self = this;
+    var trainingData = [];
+
+    var dataSize = _self.data.length;
+
+    if (dataSize < TEMPORAL_INPUT_SIZE + TEMPORAL_OUTPUT_SIZE) {
+        return;
+    }
+
+    for (var i = dataSize - 1; i >= TEMPORAL_INPUT_SIZE + TEMPORAL_OUTPUT_SIZE - 1; i--) {
+        var tempInput = [];
+        var tempOutput = [];
+
+        for (var j = 0; j < TEMPORAL_OUTPUT_SIZE; j++) {
+            var d = _self.data[i-j];
+            tempOutput.push(d["normalized"]);
+        }
+
+        for (var j = TEMPORAL_OUTPUT_SIZE; j < TEMPORAL_INPUT_SIZE + TEMPORAL_OUTPUT_SIZE; j++) {
+            var d = _self.data[i-j];
+            tempInput.push(d["normalized"]);
+        }
+
+        trainingData.push({
+            input: tempInput,
+            output: tempOutput
+        })
+    }
+
+    return trainingData;
+}
+
+Stock.prototype.trainTemporal = function () {
+
+    var _self = this;
+
+    var trainingData = _self.getTemporalNetworkInput();
+
+    //console.log(trainingData);
+
+    if (trainingData) {
+
+        var info = _self.temporalNet.train(trainingData, {
+            errorThresh: 0.005,  // error threshold to reach
+            iterations: 40000,   // maximum training iterations
+            log: false,           // console.log() progress periodically
+            logPeriod: 10       // number of iterations between logging
+        });
+
+        console.log("training "+_self.companyName+": "+JSON.stringify(info));
+
+        if (info.error == "null") {
+            console.log(trainingData);
+        }
+
+        //write weights to file
+
+        //console.log(JSON.stringify(_self.temporalNet.weights));
+        fs.writeFile("data/train/t"+_self.symbol+".json", JSON.stringify(_self.temporalNet.weights), function(err) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log("The file "+"data/train/t"+_self.symbol+".json"+" was saved!");
+            }
+        });
+
+        return JSON.stringify(info);
+    }
+
+    return;
+
+}
+
+module.exports = Stock;
